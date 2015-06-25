3a42bfe90447f85408f30fce3c713522d673e2d4
ashleymcmahon <amcmahon@amadeus.com>
2015-06-25 12:54:09 +1000

    removed reference to motd

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,7 @@ class ntp (
   validate_string($service_name)
   validate_absolute_path($logfile)
 
-  motd::register { "Ntp": }
+  #motd::register { "Ntp": }
   # Anchor this as per #8040 - this ensures that classes won't float off and
   # mess everything up.  You can read about this at:
   # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
